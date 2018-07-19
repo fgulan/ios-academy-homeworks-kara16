dcc9bd39d8feaee8cf7f5460c39fd0b24928e6c5
kara16 <filip.karacic2@gmail.com>
2018-07-19 23:22:06 +0200

    Creation of home folder in which there are storyboard and view controller and navigation to home from login screen

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -17,6 +17,25 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    @IBAction func createAccountPushHome(_ sender: Any) {
+        pushHome()
+    }
+    @IBAction func logInPushHome(_ sender: Any) {
+        pushHome()
+    }
+    
+    private func pushHome() {
+        
+        // We need to instantiate the Storyboard in which our view controller that we want to go to lives
+        let storyboard = UIStoryboard(name: "Home", bundle: nil)
+        // We need to instantiate the view controller that we want to go to
+        let viewControllerD =
+            storyboard.instantiateViewController(withIdentifier: "HomeViewController")
+        // We need to push that view controller on top of the navigation stack
+        navigationController?.pushViewController(viewControllerD, animated:
+            true)
+    }
+    
     @IBAction func checkBoxChanged(_ sender: Any) {
         checked = !checked
         
---
+++ TVShows/TVShows/New Group/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  TVShows
+//
+//  Created by Infinum Student Academy on 19/07/2018.
+//  Copyright © 2018 Filip Karacic. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
