327102cf21f7ba57d10481268741558b7a1e8630
kara16 <filip.karacic2@gmail.com>
2018-07-27 03:49:25 +0200

    Show and episode structure added

---
+++ TVShows/TVShows/Shows/ShowDetails.swift
@@ -0,0 +1,27 @@
+//
+//  ShowDetails.swift
+//  TVShows
+//
+//  Created by Infinum Student Academy on 26/07/2018.
+//  Copyright © 2018 Filip Karacic. All rights reserved.
+//
+
+import Foundation
+
+struct ShowDetails: Codable {
+    let type: String
+    let title: String
+    let description: String
+    let id: String
+    let likesCount: Int
+    let imageUrl: String
+    
+    enum CodingKeys: String, CodingKey {
+        case type
+        case title
+        case description
+        case id = "_id"
+        case likesCount
+        case imageUrl
+    }
+}
