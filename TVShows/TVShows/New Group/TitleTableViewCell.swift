90e1bb81fba4b1ae02c3ae9c5d471bb60789369a
kara16 <filip.karacic2@gmail.com>
2018-07-29 22:44:35 +0200

    Logout button added and image for each show on home page is now displayed.

--- TVShows/TVShows/New Group/TitleTableViewCell.swift
+++ TVShows/TVShows/New Group/TitleTableViewCell.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import Kingfisher
 
 class TitleTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var coverImageView: UIImageView!
     @IBOutlet weak var showTitleLabel: UILabel!
     
     func configureWith(show: Show){
         showTitleLabel.text = show.title
+        
+        let url = URL(string: "https://api.infinum.academy" + show.imageUrl)
+        coverImageView.kf.setImage(with: url)
     }
 }
