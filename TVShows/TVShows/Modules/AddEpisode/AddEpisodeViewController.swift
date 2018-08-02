6c61546961a30331c4697ef981df2f8f92e53fd4
kara16 <filip.karacic2@gmail.com>
2018-08-02 21:42:41 +0200

    Adding functionality of choosing image from gallery and uploading it as an episode image

--- TVShows/TVShows/Modules/AddEpisode/AddEpisodeViewController.swift
+++ TVShows/TVShows/Modules/AddEpisode/AddEpisodeViewController.swift
@@ -9,13 +9,18 @@
 import UIKit
 import SVProgressHUD
 import Alamofire
+import Photos
 
-protocol ShouldReload: class {
+protocol AddNewEpisodeDelegate: class {
     func shouldReloadEpisodes(episode: Episode)
 }
 
 class AddEpisodeViewController: UIViewController {
-    var delegate: ShouldReload?
+    weak var delegate: AddNewEpisodeDelegate?
+    
+    private var imagePicker = UIImagePickerController()
+    private var mediaId = ""
+    private var image: UIImage?
     
     var showId: String?
     var token: String?
@@ -27,6 +32,8 @@ class AddEpisodeViewController: UIViewController {
    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.hideKeyboardWhenTappedAround()
         
         navigationItem.title = "Add episode"
         
@@ -46,20 +53,84 @@ class AddEpisodeViewController: UIViewController {
     }
     
     @objc func didSelectAddShow() {
+        uploadImageOnAPI(token: token!, completion: {})
+    }
+    
+    @IBAction func addEpisodeImage(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
+            imagePicker.delegate = self
+            imagePicker.sourceType = .savedPhotosAlbum;
+            imagePicker.allowsEditing = false
+            
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    private func showAlert(alertMessage: String) {
+        let alertController = UIAlertController(title: "Alert", message:
+            alertMessage, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: .default,handler: nil))
         
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func uploadImageOnAPI(token: String, completion: @escaping () -> ()) {
+        let headers = ["Authorization": token]
+        
+        let someUIImage = image!
+        let imageByteData = UIImagePNGRepresentation(someUIImage)!
+         SVProgressHUD.show()
+        Alamofire
+            .upload(multipartFormData: { multipartFormData in
+                multipartFormData.append(imageByteData,
+                                         withName: "file",
+                                         fileName: "image.png",
+                                         mimeType: "image/png")
+            }, to: Constants.URL.baseUrl + "/media",
+               method: .post,
+               headers: headers)
+            { [weak self] result in
+                switch result {
+                case .success(let uploadRequest, _, _):
+                    self?.processUploadRequest(uploadRequest)
+                case .failure(let encodingError):
+                    print(encodingError)
+                }
+               completion()
+        }
+        
+        
+    }
+    
+    func processUploadRequest(_ uploadRequest: UploadRequest) {
+        uploadRequest
+            .responseDecodableObject(keyPath: "data") {[weak self] (response:
+                DataResponse<Media>) in
+        
+        switch response.result {
+
+            case .success(let media):
+                self?.mediaId = media.id
+            self?.addEpisode()
+            case .failure(let error):
+                print("FAILURE: \(error)")
+        }
+
+        }
+    }
+    
+    private func addEpisode(){
         let token: String = self.token!
         let headers = ["Authorization": token]
         
         let parameters: [String: String] = [
             "showId": showId!,
-            "mediaId": "",
+            "mediaId": mediaId,
             "title": episodeTitleTextField.text!,
             "description": episodeDescriptionTextField.text!,
             "episodeNumber": episodeNumberTextField.text!,
             "season": seasonNumberTextField.text!
         ]
-        
-        SVProgressHUD.show()
         
         Alamofire
             .request(Constants.URL.baseUrl + "/episodes",
@@ -69,27 +140,41 @@ class AddEpisodeViewController: UIViewController {
                      headers: headers)
             .validate()
             .responseDecodableObject(keyPath: "data", decoder: JSONDecoder()) {[weak self] (response:
-             DataResponse<Episode>) in
+                DataResponse<Episode>) in
                 
                 SVProgressHUD.dismiss()
                 
                 switch response.result {
                 case .success(let episode):
                     self?.delegate?.shouldReloadEpisodes(episode: episode)
-                  self?.dismiss(animated: true)
+                    self?.dismiss(animated: true)
                 case .failure(let error):
                     print(error)
                     self?.showAlert(alertMessage: "Adding episode failed")
                 }
         }
+    }
+}
+
+extension AddEpisodeViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            image = pickedImage
+            
+            
+//            if let asset = info[UIImagePickerControllerImageURL] as? URL {
+//                let str = asset.deletingPathExtension().lastPathComponent
+//
+//                print(asset.pathExtension + " " + str)
+//            }
+            
+        }
+        
+        dismiss(animated: true, completion: nil)
     }
     
-    private func showAlert(alertMessage: String) {
-        let alertController = UIAlertController(title: "Alert", message:
-            alertMessage, preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(title: "Dismiss", style: .default,handler: nil))
-        
-        present(alertController, animated: true, completion: nil)
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated:true, completion: nil)
     }
 }
